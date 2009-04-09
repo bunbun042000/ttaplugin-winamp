1280078ce2db4ff71e02f801279a89a6e2de313a
bun <bun@90ea4a2a-ca04-0410-aa38-07f7cb294ca0>
2009-04-09 03:56:46 +0000

    git-svn-id: svn+ssh://192.168.240.200/home/bun/svn/in_tta@89 90ea4a2a-ca04-0410-aa38-07f7cb294ca0

--- in_tta/in_tta.cpp
+++ in_tta/in_tta.cpp
@@ -10,9 +10,8 @@
  * Copyright (c) 2005 Alexander Djourik. All rights reserved.
  *
  */
-
 // „ÉÅEÇπ„ÉÅE
-/*
+
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
